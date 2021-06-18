c988b72788856f86d49268194724bad935eac70c
Donn <me@donn.website>
2021-06-18 13:25:14 +0200

    Squashed commit of the following:
    commit 8780865d05a8d73946fcbbbdb521a10bbbf77306
    Author: mahmoud363 <mahmoudalsayed@aucegypt.edu>
    Date:   Mon Jun 14 21:36:01 2021 +0200
    
        Fixed the password reset on AWS and added the needed configurations. Also added the captcha to the password reset endpoint

--- modules/ses_mailer.js
+++ modules/ses_mailer.js
@@ -3,10 +3,10 @@ const AWS = require("aws-sdk");
 AWS.config.update({
 	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
 	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-	region: 'eu-west-1',
-	signatureVersion: 'v4'
+	region: 'eu-central-1'
 });
 const sendMail = (template, dest, from = 'noreply@cloudv.io', params = {}) => {
+
 	return new Promise(async (resolve, reject) => {
 		const emailParams = {
 			Destination: { /* required */
@@ -22,11 +22,14 @@ const sendMail = (template, dest, from = 'noreply@cloudv.io', params = {}) => {
 			/* required */
 			ReplyToAddresses: [],
 		};
+
+		console.log(emailParams)
 		try {
 			const data = await new AWS.SES({}).sendTemplatedEmail(emailParams).promise();
+			console.log(data)
 			resolve(data);
 		} catch (err) {
-			console.error(err)
+			console.error('Error', err)
 			return reject({
 				error: 'An error has occured while sending the e-mail'
 			});
