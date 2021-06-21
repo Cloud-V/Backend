4b4386fd892c493390a0f0ae4a1d90df461cd82a
mahmoud363 <mahmoudalsayed@aucegypt.edu>
2021-06-21 23:19:01 +0200

    Fixed the bug in the google signup

--- modules/google.js
+++ modules/google.js
@@ -3,30 +3,34 @@ const { OAuth2Client } = require("google-auth-library");
 const client = new OAuth2Client(process.env.CLOUDV_GOOGLE_CLIENT_ID);
 
 async function verify(token) {
-try{
-	const ticket = await client.verifyIdToken({
-		idToken: token,
-		audience: process.env.CLOUDV_GOOGLE_CLIENT_ID
-	});
-	const payload = ticket.getPayload();
-	const {
-		sub: id,
-		name: displayName,
-		picture: avatar,
-		email,
-	} = payload;
-	const username = email.substr(0, email.lastIndexOf('@'));
-	return resolve({
-		id,
-		displayName,
-		avatar,
-		email,
-		username
-	});
-} catch (err) {
-	console.error(err);
-	return reject({ error: "Sign-in with Google failed." });
-}
+	try {
+		console.log("Verifying the token ")
+		const ticket = await client.verifyIdToken({
+			idToken: token,
+			audience: process.env.CLOUDV_GOOGLE_CLIENT_ID
+		});
+		console.log("DONW")
+		const payload = ticket.getPayload();
+		const {
+			sub: id,
+			name: displayName,
+			picture: avatar,
+			email,
+		} = payload;
+		const username = email.substr(0, email.lastIndexOf('@'));
+		return {
+			id,
+			displayName,
+			avatar,
+			email,
+			username
+		};
+	} catch (err) {
+		console.error(err);
+		var ts = Math.round((new Date()).getTime() / 1000);
+		console.error(ts);
+		return reject({ error: "Sign-in with Google failed." });
+	}
 }
 
 module.exports = {
